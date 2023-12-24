36841e3a5847eac6276a4e32241acc8662f35244
tesh-ts <ricoigl5802@gmail.com>
2023-12-25 00:58:57 +0300

    resolved merge conflicts or any other message

--- web/views/worker_view.php
+++ web/views/worker_view.php
@@ -1,140 +1,3 @@
-<!-- <?php
-
-
-if (!isset($_SESSION['username'])) {
-    header("Location: ../controllers/login_controller.php");
-    exit();
-}
-
-$getToursStmt = $conn->prepare("SELECT * FROM tours");
-$getToursStmt->execute();
-$tours = $getToursStmt->get_result()->fetch_all(MYSQLI_ASSOC);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Личный кабинет сотрудника</title>
-    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            background-color: #f4f4f4;
-            margin: 20px;
-        }
-
-        h1 {
-            color: #333;
-        }
-
-        ol {
-            list-style: none;
-            padding: 0;
-        }
-
-        li {
-            margin-bottom: 5px;
-        }
-
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 10px;
-        }
-
-        th, td {
-            padding: 10px;
-            border: 1px solid #ddd;
-            text-align: left;
-            color: black;
-        }
-
-        th {
-            background-color: #28612e;
-        }
-
-        h1 {
-            margin-top: 50px;
-            color: black;
-            position: relative;
-            font-weight: bold;
-        }
-
-
-    </style>
-</head>
-<body>
-    
-    <h1>Личный кабинет</h1>
-    <p>Привет, <?php echo $_SESSION['username']; ?>!</p>
-    <h2>Доступные туры:</h2>
-    <ul>
-        <?php foreach ($tours as $tour): ?>
-            <li>
-                <?php echo $tour['id'] . '. Страна: ' . $tour['country'] . ', Город: ' . $tour['town'] . ' , Продолжительность: ' . $tour['duration'] . ' , Цена: ' . $tour['price'] . '$, Поставщик: ' . $tour['provider_id']; ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-
-    <h2>Форма добавления нового тура</h2>
-    <form method="post">
-        <input type="text" name="new_country" placeholder="Country">
-        <input type="text" name="new_town" placeholder="Town">
-        <input type="text" name="new_duration" placeholder="Duration">
-        <input type="text" name="new_price" placeholder="Price">
-        <input type="text" name="new_provider_id" placeholder="Provider_ID">
-        <button type="submit" name="add_tour">Добавить тур</button>
-    </form>
-
-    <h2>Форма обновления информации о туре</h2>
-    <form method="post">
-        <input type="text" name="id_to_update" placeholder="Id to Update"> 
-        <input type="text" name="updated_country" placeholder="Updated Country">
-        <input type="text" name="updated_town" placeholder="Updated Town">
-        <input type="text" name="updated_duration" placeholder="Updated Duration">
-        <input type="text" name="updated_price" placeholder="Updated Price">
-        <input type="text" name="updated_provider_id" placeholder="Updated Provider_ID">
-        <button type="submit" name="update_tour">Обновить тур</button>
-    </form>
-
-    <h2>Форма удаления тура</h2>
-    <form method="post">
-    <input type="text" name="id_to_delete" placeholder="Id to Delete"> 
-        <button type="submit" name="delete_tour">Удалить тур</button>
-    </form>
-    <p><a href="../logout.php">Выйти из учетной записи</a></p> <br>
-    <p><a href="../index.php">Вернуться на главную страницу</a></p>
-
-    <div id="full"></div>
-    <script>
-
-        function updatePage() {
-            $.ajax({
-                type: 'POST',
-                url: 'worker_controller.php',
-                data: { },
-                success: function(response) {
-                    $('#full').empty().html(response);
-                },
-                error: function(error) {
-                    console.error('Ошибка AJAX:', error);
-                }
-            });
-        }
-
-
-        updatePage();
-        
-    </script>
-
-</body>
-</html> -->
-
-
-<!-- worker_view.php -->
 <?php
 if (!isset($_SESSION['username'])) {
     header("Location: ../controllers/login_controller.php");
@@ -145,9 +8,9 @@ $getToursStmt = $conn->prepare("SELECT * FROM tours");
 $getToursStmt->execute();
 $tours = $getToursStmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
-// Проверяем, был ли запрос через AJAX
+
 if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
-    // Этот код будет выполнен только при AJAX-запросе
+
     ?>
     <h2>Доступные туры:</h2>
     <ul>
@@ -158,7 +21,7 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
         <?php endforeach; ?>
     </ul>
     <?php
-    // Завершаем выполнение, чтобы избежать вывода полной HTML-страницы
+
     exit();
 }
 ?>
